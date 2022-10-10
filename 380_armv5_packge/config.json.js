a4d7744200ac1d48c50b04795a40522b14b2c5e7
cary-sas <61653855+cary-sas@users.noreply.github.com>
2022-10-10 23:34:22 +0800

    v4.36.6

---
+++ 380_armv5_packge/config.json.js
@@ -0,0 +1,9 @@
+{
+"build_date":"2022-10-10_23:34:20",
+"description":"科学上网",
+"home_url":"Main_Ss_Content.asp",
+"md5":"060733aae0a6857b5bc04b9c5dc91e20",
+"name":"shadowsocks",
+"tar_url": "https://github.com/cary-sas/v2ray_bin/raw/main/380_armv5_packge/4.36.6/shadowsocks.tar.gz",
+"version":"4.36.6"
+}
